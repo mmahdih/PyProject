02ead3b20db12f9216fa891f9fa1fb6bfabeec00
mmahdih <mahdi.haidary.sinc@gmail.com>
2025-03-20 12:13:05 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,83 @@
+# print("Hello World!")
+#
+# # in python, you don't need to write the variable type
+# a = 3
+# b = 12.2
+#
+# coddy = "I am learing to code with Coody!"
+# print(f'coddy = "{coddy}"')
+#
+# for i in range(5):
+#     print(i)
+#
+#
+# sum = 0
+# for i in range(100):
+#     sum += i
+#
+# print(sum)
+#
+# a = 5
+# a += 3
+# if a == 5:
+#     print(f"Error")
+# elif a < 3:
+#     print(f"small")
+# else:
+#     print(f"{a}")
+#
+# wind = int(input())
+# status = "sunset"
+#
+# if wind < 8:
+#     status = "Calm"
+# elif wind > 8 and wind < 31:
+#     status = "Breeze"
+# elif wind > 32 and wind < 63:
+#     status = "Gale"
+# else:
+#     status = "Storm"
+#
+# print(f"status = {status}")
+#
+# var = int(input())
+# var = 120 - var
+# print(f"{var} years till 120")
+
+
+# def sum(a, b):
+#     c = a + b
+#     print(f"the sum of {a} and {b} is: {c}")
+#
+# a = int(input())
+# b = int(input())
+# sum(a, b)
+
+
+# def sum_numbers():
+#     res = 0
+#     for i in range(1, 10001):
+#         res += i
+#     print(res)
+#
+# n = int(input())
+# for i in range(n):
+#     sum_numbers()
+
+
+# def is_even (number):
+#     if number % 2 == 0:
+#         print(f"{number} is even")
+#     else:
+#         print(f"{number} is odd")
+#
+# for i in range(12,24):
+#     is_even(i)
+
+
+# def multiply (n1, n2):
+#     return n1 * n2
+#
+# number = multiply(2,4)
+# print(number)
+
